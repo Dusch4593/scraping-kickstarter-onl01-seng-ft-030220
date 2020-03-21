7cc05e47ad2cd59daaf6d89045ddb410297a6e62
Brandon Dusch <brandondusch@gmail.com>
2020-03-21 13:58:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,7 +16,7 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text.strip
     binding.pry
     project_hash[title.to_sym] = {
-      image_link => project.css("div.project-thumbnail a img").attribute("src").value, 
+      image_link: project.css("div.project-thumbnail a img").attribute("src").value, 
       
     }
   end
