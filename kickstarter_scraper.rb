147072a8e1ac4d53c34d040191e617f48d00bd20
Brandon Dusch <brandondusch@gmail.com>
2020-03-21 14:04:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,7 +18,7 @@ def create_project_hash
     project_hash[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value, 
       :description => project.css("p.bbcard_blurb").text.strip, 
-      :location => 
+      #:location => 
     }
   end
   
